b86c7a5dd677c65dbc5d558f5b9c7459af90e91d
Anthony <yihonganthony@gmail.com>
2020-06-13 12:42:47 -0500

    v1.2.0: customizable quizzes, more complete scoring system, and feature to share your quiz with friends

---
+++ application/src/app/components/challenge/challenge.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ChallengeComponent } from './challenge.component';
+
+describe('ChallengeComponent', () => {
+  let component: ChallengeComponent;
+  let fixture: ComponentFixture<ChallengeComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ChallengeComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ChallengeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- application/src/app/components/score/score.component.ts
+++ application/src/app/components/score/score.component.ts
@@ -1,6 +1,21 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute, ParamMap  } from '@angular/router';
 import { SpotifyService } from '../services/spotify.service';
+import { DatastoreService } from '../services/datastore.service';
+
+// function for a random int
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+function generateid() {
+  var chars = "abcdefghijklmnopqrstuvwxyz1234567890"
+  var ret = ""
+  for (var i = 0; i < 5; i++) {
+    ret += chars[getRandomInt(chars.length)];
+  }
+  return (ret);
+}
 
 @Component({
   selector: 'app-score',
@@ -10,7 +25,7 @@ import { SpotifyService } from '../services/spotify.service';
 })
 export class ScoreComponent implements OnInit {
 
-  constructor(private router: Router, private spotify: SpotifyService) { }
+  constructor(private router: Router, private route: ActivatedRoute, private spotify: SpotifyService, private data: DatastoreService) { }
 
   score = NaN;
   token = "";
@@ -22,21 +37,112 @@ export class ScoreComponent implements OnInit {
   submitted = false;
   isCorrect = false;
   percent = null;
+  config = {}
+  challengeObj = {}
+  hasTracks = false;
+  displayname = "Host";
+  challengeCode = "";
+  enteredName = "";
+  hostName = "";
+  leaderboard = [];
+  isGenerated = false;
+  currentLink = "";
+  urlchange;
   sentDB;
 
   ngOnInit() {
+    this.isGenerated = false;
     this.percent = sessionStorage.getItem("percent") || 50;
     this.sentDB = sessionStorage.getItem("sentDB");
-    this.score = Number(sessionStorage.getItem("score")) || 0;
-    this.token = sessionStorage.getItem("token");
-    //console.log(this.token);
+    this.enteredName = sessionStorage.getItem("enteredName") || "You";
+    this.score = Number(sessionStorage.getItem("score")) || -1;
+    this.currentLink = sessionStorage.getItem("currentLink") || "";
+    //this.spotify.cleanChallenges(Date.now() - 180000000);
+    this.urlchange = this.route.params.subscribe(params => {
+      this.challengeCode = params["code"] || ""; // gets code from url
+      if (this.challengeCode != "") {
+        this.spotify.getChallenge(this.challengeCode).then(
+          res => {
+            let tmpboard = res["scoreboard"].slice();
+            if (this.sentDB == "no" && this.score >= 0) {
+              // If score from current session not posted to db, use the session stored score for leaderboard
+              tmpboard.push({name: this.enteredName, score: this.score, me: true});
+              this.leaderboard = tmpboard.sort((a, b) => (a.score > b.score) ? -1 : 1);
+            } else {
+              // if already posted, use the scoreboard from the db, avoid duplicates
+              this.leaderboard = tmpboard.sort((a, b) => (a.score > b.score) ? -1 : 1);
+              this.hostName = tmpboard.find(obj => obj.host === true).name;
+            }
+            if (this.sentDB == "no") {
+              // POST the challenger score to the db
+              let toPostBoard = res["scoreboard"].slice();
+              toPostBoard.push({name: this.enteredName, score: this.score});
+              this.spotify.addChallengeScore(this.challengeCode, toPostBoard);
+              sessionStorage.setItem("sentDB", "yes")
+            }
+          }).catch((e) => {
+            console.log(e);
+            this.router.navigate(["/"]);
+          });
+      } else {
+        this.token = sessionStorage.getItem("token");
+        if (this.score < 0) {
+          this.router.navigate(["/"]);
+        }
+        this.challengeObj = {
+          "_id": generateid(),
+          "time": Date.now(),
+          "whisperLen": 5, 
+          "timeLimit": 20, 
+          "tracks": [], 
+          "scoreboard": [], 
+          "tracklist": [], 
+          "indexes": []
+        }
+        this.data.currentConfig.subscribe(configs => {
+          if (configs == "default") {
+            //this.router.navigate(["/welcome"]);
+          } else {
+            this.config = configs;
+            this.challengeObj["whisperLen"] = this.config["whisperLen"];
+            this.challengeObj["timeLimit"] = this.config["timeLimit"];
+          }
+        });
+        this.data.currentSongs.subscribe(songs => {
+          if (songs == "default") {
+            //this.router.navigate(["/welcome"]);
+          } else {
+            this.challengeObj["tracks"] = songs;
+            this.hasTracks = true;
+          }
+        });
+        this.data.currentSongList.subscribe(songList => {
+          if (songList == "default") {
+            //this.router.navigate(["/welcome"]);
+          } else {
+            this.challengeObj["tracklist"] = songList;
+          }
+        });
+        this.data.currentIndexes.subscribe(indList => {
+          if (indList == "default") {
+            //this.router.navigate(["/welcome"]);
+          } else {
+            this.challengeObj["indexes"] = indList;
+          }
+        });
+        this.getSongsAndPostToDB();
+      }
+    });
+  }
+
+  getSongsAndPostToDB() {
     // checks that token is present and we haven't already sent this quiz round to DB
     if (this.token != "" && this.token != null && this.sentDB == "no") {
     // get request
       sessionStorage.setItem("sentDB", "yes");
-      this.spotify.getTracks(this.token, "0").then(
+      this.spotify.getTracks(this.token, "0", "medium_term").then(
         res => {
-          this.spotify.getTracks(this.token, "49").then(
+          this.spotify.getTracks(this.token, "49", "medium_term").then(
             res2 => {
           //console.log(res);
           this.trackprev = res['items'].concat(res2["items"]);
@@ -89,6 +195,7 @@ export class ScoreComponent implements OnInit {
                   score: this.score
                 }
               });*/
+              this.displayname = useres["display_name"];
               this.spotify.addEntry({
                   _id: useres["id"], 
                   name: useres["display_name"], 
@@ -98,6 +205,7 @@ export class ScoreComponent implements OnInit {
                   score: this.score
               });
               //console.log("sent")
+              this.challengeObj["scoreboard"].push({name: this.displayname, score: this.score, host: true});
               // takes the response from sending score to database and calculates percentage of users beaten
               this.spotify.addScore(this.score).then(
                 resp => {
@@ -114,9 +222,72 @@ export class ScoreComponent implements OnInit {
       }
   }
 
+  openShare() {
+    this.isGenerated = true;
+    if (this.currentLink == "") {
+      this.generateLink();
+    }
+  }
+
+  generateLink(retries = 5) {
+    // insert this object to database, regenerating code if doesn't work.
+    //console.log(this.challengeObj);
+    if (retries > 0) {
+      //console.log(retries);
+      this.spotify.addChallenge(this.challengeObj).then(
+        resp => {
+          //console.log(this.challengeObj["tracks"])
+          //console.log(resp);
+          this.currentLink = this.challengeObj["_id"];
+          sessionStorage.setItem("currentLink", this.currentLink);
+          return;
+        }
+      ).catch((e) => {
+        //console.log(e)
+        this.challengeObj["_id"] = generateid();
+        return this.generateLink(--retries);
+      });
+    } else {
+      console.log("failed all retries");
+      this.isGenerated = false;
+    }
+    // after has been inserted, CONFIRM success and load the popup
+    // result link should be a different challenge-score page, should work immediately
+    
+  }
+
+  unLoad() {
+    this.isGenerated = false;
+  }
+
+  copyLink(val) {
+    const selBox = document.createElement('textarea');
+    selBox.style.position = 'fixed';
+    selBox.style.left = '0';
+    selBox.style.top = '0';
+    selBox.style.opacity = '0';
+    selBox.value = val;
+    selBox.setAttribute('readonly', 'true');
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    selBox.setSelectionRange(0, 99999);
+    document.execCommand('copy');
+    document.body.removeChild(selBox);
+    // another way to write to clipboard  but idk if the above one already works
+    navigator.clipboard.writeText(val);
+  }
 
   playAgain() {
     sessionStorage.setItem("score", "NaN");
-    this.router.navigate(["/welcome"]);
+    if (this.challengeCode == "" || this.currentLink != "") {
+      this.router.navigate(["/welcome"]);
+    } else {
+      this.router.navigate(["/challenge", this.challengeCode]);
+    }
+  }
+
+  ngOnDestroy() {
+    this.urlchange.unsubscribe();
   }
 }
